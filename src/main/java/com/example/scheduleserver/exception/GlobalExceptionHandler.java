09b69b3794442b3674c2bf29cad2bc22351722ac
euuns <dmsu2@naver.com>
2025-02-09 21:25:04 +0900

    Comment: GlobalExceptionHandler 주석 추가

--- src/main/java/com/example/scheduleserver/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/scheduleserver/exception/GlobalExceptionHandler.java
@@ -15,11 +15,16 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    // Filed Error 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorResponseDto>> violationException(MethodArgumentNotValidException e) {
         List<ErrorResponseDto> errors = new ArrayList<>();
+
+        // 생성된 필드 에러를 모두 가져옴
         List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
 
+        // 에러 정보를 반환하기 위해 ErrorResponseDto에 발생 필드와 메세지 저장
         for (FieldError err:fieldErrors) {
             errors.add(new ErrorResponseDto(err.getField(), err.getDefaultMessage()));
         }
@@ -28,8 +33,11 @@ public class GlobalExceptionHandler {
 
     }
 
+
+    // Request와 login한 유저(Session을 받은 유저) 일치하지 않는 예외 처리
     @ExceptionHandler(SessionUserNotEqualsException.class)
     public ResponseEntity<String> sessionNotEqualException(SessionUserNotEqualsException e){
+        // BAD_REQUEST와 함께 에러 메세지 출력
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
