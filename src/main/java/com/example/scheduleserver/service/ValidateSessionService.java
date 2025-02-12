ea4991fa8fb78b379cf9156392259f509981d1f2
euuns <dmsu2@naver.com>
2025-02-12 14:04:01 +0900

    Style: Change parameter name

--- src/main/java/com/example/scheduleserver/service/ValidateSessionService.java
+++ src/main/java/com/example/scheduleserver/service/ValidateSessionService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class ValidateSessionService {
 
-    public void validateSessionUser(Long sessionUserId, Long scheduleUserId) {
-        if (!sessionUserId.equals(scheduleUserId)) {
+    public void validateSessionUser(Long sessionUserId, Long requestUserId) {
+        if (!sessionUserId.equals(requestUserId)) {
             throw new ValidException(ExceptionCode.SESSION_NOT_VALID);
         }
     }
